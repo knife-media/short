a694fada1d662a6ab831349b08d5657b41dcc8db
Anton Lukin <anton@lukin.me>
2022-05-16 20:52:12 -0300

    add social sources endpoint

--- dump.sql
+++ dump.sql
@@ -1,17 +1,31 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `social`;
+CREATE TABLE `social` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `keyword` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `clicks` int unsigned NOT NULL DEFAULT '0',
+  `source` set('tg','fb','vk','ok') CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `keyword_social` (`keyword`,`source`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
 DROP TABLE IF EXISTS `urls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `urls` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `keyword` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
-  `url` text CHARACTER SET utf8mb4 NOT NULL,
-  `title` text CHARACTER SET utf8mb4,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `keyword` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `ip` varchar(41) CHARACTER SET utf8mb4 NOT NULL,
-  `clicks` int(10) unsigned NOT NULL,
+  `ip` varchar(41) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `clicks` int unsigned NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `keyword` (`keyword`),
-  KEY `timestamp` (`timestamp`),
-  KEY `ip` (`ip`)
-) ENGINE=InnoDB AUTO_INCREMENT=3857 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
---
+++ server.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+
+const express = require('express');
+const database = require('./utils/database');
+
+const app = express();
+
+/**
+ * Redirect knife.media posts with social sources
+ */
+app.get('^/:source(tg|vk|fb|ok)/:keyword(*)', async (req, res, next) => {
+  try {
+    if (!req.params.keyword) {
+      return next();
+    }
+
+    // We make sure links are case insensitive
+    const keyword = req.params.keyword.toLowerCase();
+
+    if (!keyword.match(/^[a-z0-9-_\/]+$/)) {
+      return next();
+    }
+
+    const sources = {
+      tg: '?utm_medium=telegram',
+      vk: '?utm_medium=vkontakte',
+      ok: '?utm_medium=odnoklassniki',
+      fb: '?utm_medium=facebook'
+    };
+
+    const query = 'INSERT INTO social (keyword, source) VALUES (?, ?) ON DUPLICATE KEY UPDATE clicks = clicks + 1';
+
+    // Make database query
+    await database.query(query, [keyword, req.params.source]);
+
+    res.redirect('https://knife.media/' + keyword + sources[req.params.source]);
+  } catch(err) {
+    return next(err);
+  }
+});
+
+/**
+ * Route primary named location
+ */
+app.get('^/:keyword([a-z0-9-]{1,200})/?$', async (req, res, next) => {
+  try {
+    const [rows] = await database.query('SELECT * FROM urls WHERE keyword = ? LIMIT 1', [req.params.keyword]);
+
+    if (rows.length === 0) {
+      return next();
+    }
+
+    // Make database query
+    await database.query('UPDATE urls SET clicks = clicks + 1 WHERE keyword = ?', [req.params.keyword]);
+
+    res.redirect(rows[0].url);
+  } catch(err) {
+    return next(err);
+  }
+});
+
+/**
+ * Handle errors
+ */
+app.use((err, req, res, next) => {
+  if (err.message) {
+    console.error(err.message);
+  }
+
+  return next();
+});
+
+/**
+ * Show default index template
+ */
+app.use((req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+/**
+ * Let's roll
+ */
+app.listen(process.env.PORT || 3000);
